5b84eff7bad659511a26fec92179ca8bafe6dd13
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2023-04-21 19:24:50 +0600

    logout

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { Route, Routes } from "react-router-dom"
 import Register from "./components/Pages/Authentication/Register"
 import LogIn from "./components/Pages/Authentication/LogIn"
 import Home from "./components/Pages/home/Home"
+import Navbar from "./components/shared/Navbar"
 
 function App() {
 
   return (
     <>
+    <Navbar />
       <Routes>
         <Route path='/' element={<Home />}></Route>
         <Route path='/home' element={<Home />}></Route>
