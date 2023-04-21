9cdafd1ff60490583045ad6f7aac5b8e1f3bba61
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2023-04-21 17:11:32 +0600

    login

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom"
 import Register from "./components/Pages/Authentication/Register"
+import LogIn from "./components/Pages/Authentication/LogIn"
 
 function App() {
 
   return (
     <>
       <Routes>
+        <Route path="/login" element={<LogIn/>}></Route>
         <Route path="/register" element={<Register/>}></Route>
       </Routes>
     </>
--- src/firebase.init.jsx
+++ src/firebase.init.jsx
@@ -1,18 +1,18 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getAuth} from 'firebase/auth';
+import { getAuth } from 'firebase/auth';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.REACT_APP_API_KEY,
-  authDomain: import.meta.env.REACT_APP_AUTH_DOMAIN,
-  projectId: import.meta.env.REACT_APP_PROJECT_ID,
-  storageBucket: import.meta.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: import.meta.env.REACT_APP_APP_ID,
+  apiKey: "AIzaSyDF0_qn4SMKx-gBNaS-BX7uvMAzBBsflSk",
+  authDomain: "quick-start-7efb6.firebaseapp.com",
+  projectId: "quick-start-7efb6",
+  storageBucket: "quick-start-7efb6.appspot.com",
+  messagingSenderId: "502689170391",
+  appId: "1:502689170391:web:287bf123a79d60645d0192"
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
