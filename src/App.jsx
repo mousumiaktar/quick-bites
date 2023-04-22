ee668b8467ff77d27e1fbca86b3f7fc836cbb696
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2023-04-22 15:22:07 +0600

    dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,28 @@
-import { Route, Routes } from "react-router-dom"
+import { Route, Routes} from "react-router-dom"
 import Register from "./components/Pages/Authentication/Register"
 import LogIn from "./components/Pages/Authentication/LogIn"
 import Home from "./components/Pages/home/Home"
 import Navbar from "./components/shared/Navbar"
+import Dashboard from "./components/dashboard/Dashboard"
+import MyOrder from "./components/dashboard/MyOrder"
+import AddProducts from "./components/dashboard/AddProducts"
 
 function App() {
+  
 
   return (
     <>
-    <Navbar />
+      <Navbar />
       <Routes>
         <Route path='/' element={<Home />}></Route>
         <Route path='/home' element={<Home />}></Route>
         <Route path="/login" element={<LogIn />}></Route>
         <Route path="/register" element={<Register />}></Route>
+        <Route path="/dashboard" element={<Dashboard />}>
+          <Route path="orders" element={<MyOrder />}></Route>
+          <Route path="add-products" element={<AddProducts />}></Route>
+        </Route>
+
       </Routes>
     </>
   )
---
+++ src/components/dashboard/MyOrder.jsx
@@ -0,0 +1,9 @@
+const MyOrder = () => {
+    return (
+        <div>
+            <h1 className="text-xl font-bold">This is my order page</h1>
+        </div>
+    );
+};
+
+export default MyOrder;
