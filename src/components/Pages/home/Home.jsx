4e85cd416e91bc9e552805a2adb625e6db0d6f04
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2023-04-23 19:22:21 +0600

    restaurants

--- src/components/Pages/home/Home.jsx
+++ src/components/Pages/home/Home.jsx
@@ -3,12 +3,14 @@ import HowItWorks from "./OthersDesign/HowItWorks";
 import MobileApp from "./OthersDesign/MobileApp";
 import SearchHero from "./OthersDesign/SearchHero";
 import TwoCart from "./OthersDesign/TwoCart";
+import Restaurants from "./Restaurants";
 
 const Home = () => {
     return (
         <div>
             <SearchHero />
             <HowItWorks />
+            <Restaurants />
             <TwoCart />
             <About />
             <MobileApp />
