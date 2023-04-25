e3bc04260d661db301a4b00c6c980abf43fd0179
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2023-04-25 14:48:20 +0600

    show all restaurant

--- src/components/Pages/home/Restaurants.jsx
+++ src/components/Pages/home/Restaurants.jsx
@@ -1,73 +1,47 @@
-import { AiFillStar } from "react-icons/ai";
+import { AiFillStar, } from "react-icons/ai";
+import { useGetRestaurantsQuery } from '../../../features/api/ApiSlice';
+import Loading from '../../shared/Loading';
+// import { useNavigate } from 'react-router-dom';
+
+
+
 
 const Restaurants = () => {
+    // const navigate = useNavigate();
+
+    const { data, isLoading, } = useGetRestaurantsQuery();
+    if (isLoading) {
+        <Loading />
+    }
+    // if (isSuccess) {
+    //   console.log(data);
+    // }
+
+
     return (
         <div>
-            <div className="grid lg:grid-cols-4  md:grid-cols-3 sm:grid-cols-1  px-16 py-6 gap-4">
-                <div className="card shadow-xl">
-                    <img
-                        src="https://images.deliveryhero.io/image/fd-bd/LH/y3yt-listing.jpg?width=400&height=292"
-                        alt=""
-                    />
-                    <div className="mt-2 p-2">
-                        <div className="flex justify-between items-center">
-                            <h1 className="font-bold">Ridima Restaurant</h1>
-                            <div className="flex gap-2 items-center">
-                                <AiFillStar className="text-yellow-500"></AiFillStar>
-                                <p>3.7</p>
+            <h1 className="text-center text-3xl py-2 font-bold">Featured Restaurants</h1>
+            <div className="grid lg:grid-cols-4  md:grid-cols-3 sm:grid-cols-2 xs:grid-cols-2  px-16 py-6 gap-8">
+                {data?.map((data) => (
+                    <>
+                        <div
+                            className="card shadow-xl"
+                            
+                        >
+                            <img src={data.resimg} alt="" />
+                            <div className="mt-2 p-2">
+                                <div className="flex justify-between items-center">
+                                    <h1 className="font-bold">{data.resname}</h1>
+                                    <div className="flex gap-2 items-center">
+                                        <AiFillStar className="text-yellow-500"></AiFillStar>
+                                        <p>{data.rank}</p>
+                                    </div>
+                                </div>
+                                <h1>{data.location}</h1>
                             </div>
                         </div>
-                        <h1>Dhaka</h1>
-                    </div>
-                </div>
-                <div className="card shadow-xl">
-                    <img
-                        src="https://images.deliveryhero.io/image/fd-bd/LH/y3yt-listing.jpg?width=400&height=292"
-                        alt=""
-                    />
-                    <div className="mt-2 p-2">
-                        <div className="flex justify-between items-center">
-                            <h1 className="font-bold">Ridima Restaurant</h1>
-                            <div className="flex gap-2 items-center">
-                                <AiFillStar className="text-yellow-500"></AiFillStar>
-                                <p>3.7</p>
-                            </div>
-                        </div>
-                        <h1>Dhaka</h1>
-                    </div>
-                </div>
-                <div className="card shadow-xl">
-                    <img
-                        src="https://images.deliveryhero.io/image/fd-bd/LH/y3yt-listing.jpg?width=400&height=292"
-                        alt=""
-                    />
-                    <div className="mt-2 p-2">
-                        <div className="flex justify-between items-center">
-                            <h1 className="font-bold">Ridima Restaurant</h1>
-                            <div className="flex gap-2 items-center">
-                                <AiFillStar className="text-yellow-500"></AiFillStar>
-                                <p>3.7</p>
-                            </div>
-                        </div>
-                        <h1>Dhaka</h1>
-                    </div>
-                </div>
-                <div className="card shadow-xl">
-                    <img
-                        src="https://images.deliveryhero.io/image/fd-bd/LH/y3yt-listing.jpg?width=400&height=292"
-                        alt=""
-                    />
-                    <div className="mt-2 p-2">
-                        <div className="flex justify-between items-center">
-                            <h1 className="font-bold">Ridima Restaurant</h1>
-                            <div className="flex gap-2 items-center">
-                                <AiFillStar className="text-yellow-500"></AiFillStar>
-                                <p>3.7</p>
-                            </div>
-                        </div>
-                        <h1>Dhaka</h1>
-                    </div>
-                </div>
+                    </>
+                ))}
             </div>
         </div>
     );
