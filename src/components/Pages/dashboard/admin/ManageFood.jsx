a7cd33968ffde219b62e2cd37e3e62248ca8c8ce
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2023-04-28 23:12:10 +0600

    get all food

--- src/components/Pages/dashboard/admin/ManageFood.jsx
+++ src/components/Pages/dashboard/admin/ManageFood.jsx
@@ -1,25 +1,39 @@
+import { useGetAllFoodQuery } from "../../../../features/api/ApiSlice";
+import Loading from "../../../shared/Loading";
+
 const ManageFood = () => {
+    const { data, isLoading, } = useGetAllFoodQuery();
+    console.log(data);
+    if (isLoading) {
+        <Loading />
+    }
+
+
     return (
         <div className="overflow-x-auto">
             <table className="table-auto w-full">
                 <thead>
                     <tr>
+                        <th className="px-4 py-2"></th>
+                        <th className="px-4 py-2">Image</th>
                         <th className="px-4 py-2">Food-Name</th>
                         <th className="px-4 py-2">Price</th>
                         <th className="px-4 py-2">Food-Type</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td className="border px-4 py-2">Cold Chicken Sandwich</td>
-                        <td className="border px-4 py-2">110 TK.</td>
-                        <td className="border px-4 py-2">Sandwich</td>
-                    </tr>
-                    <tr>
-                        <td className="border px-4 py-2">Cold Chicken Sandwich</td>
-                        <td className="border px-4 py-2">110 TK.</td>
-                        <td className="border px-4 py-2">Sandwich</td>
-                    </tr>
+                    {
+                        data?.map((food, index) => <tr>
+                            <th>{index + 1}</th>
+                            <td className="border px-4 py-2">
+                                <img className='h-12' src={food.fimg} alt='' />
+                            </td>
+                            <td className="border px-4 py-2">{food.fname}</td>
+                            <td className="border px-4 py-2">{food.price}</td>
+                            <td className="border px-4 py-2">{food.ftype}</td>
+                        </tr>)
+                    };
+
                 </tbody>
             </table>
         </div>
