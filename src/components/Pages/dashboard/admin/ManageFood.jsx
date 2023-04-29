8086d6860d7e1300adefdecf257e4f93bc753b60
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2023-04-29 12:35:31 +0600

    bug fix

--- src/components/Pages/dashboard/admin/ManageFood.jsx
+++ src/components/Pages/dashboard/admin/ManageFood.jsx
@@ -30,34 +30,39 @@ const ManageFood = () => {
 
 
     return (
-        <div className="overflow-x-auto">
+        <div className="overflow-x-auto h-screen pt-8">
             <table className="table-auto w-full">
-                <thead>
+                <thead className="bg-red-500">
                     <tr>
-                        <th className="px-4 py-2"></th>
+                        <th className="px-4 py-2">Index</th>
                         <th className="px-4 py-2">Image</th>
                         <th className="px-4 py-2">Food-Name</th>
                         <th className="px-4 py-2">Price</th>
                         <th className="px-4 py-2">Food-Type</th>
+                        <th className="px-4 py-2">Action</th>
                     </tr>
                 </thead>
                 <tbody>
                     {
-                        data?.map((food, index) => <tr>
-                            <th>{index + 1}</th>
-                            <td className="border px-4 py-2">
-                                <img className='h-12' src={food.fimg} alt='' />
-                            </td>
-                            <td className="border px-4 py-2">{food.fname}</td>
-                            <td className="border px-4 py-2">{food.price} TK.</td>
-                            <td className="border px-4 py-2">{food.ftype}</td>
-                            <td className="border px-4 py-2"><button
-                                onClick={() => handleDeletefood(food._id)}
-                                className='bg-red-500 px-2 rounded text-white'
-                            >
-                                DELETE
-                            </button></td>
-                        </tr>)
+                        data?.map((food, index) => (
+                            <>
+                                <tr>
+                                    <th className="border px-4 py-2">{index + 1}</th>
+                                    <td className="border px-4 py-2">
+                                        <img className='h-12' src={food.fimg} alt='' />
+                                    </td>
+                                    <td className="border px-4 py-2">{food.fname}</td>
+                                    <td className="border px-4 py-2">{food.price} TK.</td>
+                                    <td className="border px-4 py-2">{food.ftype}</td>
+                                    <td className="border px-4 py-2"><button
+                                        onClick={() => handleDeletefood(food._id)}
+                                        className='bg-red-500 px-2 rounded text-white'
+                                    >
+                                        DELETE
+                                    </button></td>
+                                </tr>
+                            </>
+                        ))
                     };
 
                 </tbody>
