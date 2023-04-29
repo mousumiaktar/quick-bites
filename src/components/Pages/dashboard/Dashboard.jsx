ce5774e8622db5dd9445de9ac16971fb97e2831a
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2023-04-29 15:34:33 +0600

    dashboard updated

--- src/components/Pages/dashboard/Dashboard.jsx
+++ src/components/Pages/dashboard/Dashboard.jsx
@@ -7,32 +7,32 @@ const Dashboard = () => {
                 <div className="col-start-1 col-end-3 bg-black py-6">
                     <div className="">
                         <Link to="profile">
-                            <h1 className="font-bold  p-2 px-6 text-white text-xl mb-3">
+                            <h1 className="font-bold  p-2 px-6 text-white text-xl mb-3 hover:bg-orange-400 mx-3 rounded-md">
                                 profile
                             </h1>
                         </Link>
                         <Link to="addreview">
-                            <h1 className="font-bold  p-2 px-6 text-white text-xl mb-3">
+                            <h1 className="font-bold  p-2 px-6 text-white text-xl mb-3 hover:bg-orange-400 mx-3 rounded-md">
                                 Reviews
                             </h1>
                         </Link>
                         <Link to="myorder">
-                            <h1 className="font-bold  p-2 px-6 text-white text-xl mb-3">
+                            <h1 className="font-bold  p-2 px-6 text-white text-xl mb-3 hover:bg-orange-400 mx-3 rounded-md">
                                 Orders
                             </h1>
                         </Link>
                         <Link to="addrestaurant">
-                            <h1 className="font-bold  p-2 px-6 text-white text-xl mb-3">
+                            <h1 className="font-bold  p-2 px-6 text-white text-xl mb-3 hover:bg-orange-400 mx-3 rounded-md">
                                 Add Restaurants
                             </h1>
                         </Link>
                         <Link to="managefood">
-                            <h1 className="font-bold  p-2 px-6 text-white text-xl mb-3">
+                            <h1 className="font-bold  p-2 px-6 text-white text-xl mb-3 hover:bg-orange-400 mx-3 rounded-md">
                                 Manage Food
                             </h1>
                         </Link>
                         <Link to="makeadmin">
-                            <h1 className="font-bold  p-2 px-6 text-white text-xl mb-3">
+                            <h1 className="font-bold  p-2 px-6 text-white text-xl mb-3 hover:bg-orange-400 mx-3 rounded-md">
                                 Make Admin
                             </h1>
                         </Link>
